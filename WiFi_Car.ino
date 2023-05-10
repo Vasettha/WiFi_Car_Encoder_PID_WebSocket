0419b4f81e8bcffeee31ac5b769f92529ff764bb
Vasettha <95194173+Vasettha@users.noreply.github.com>
2023-05-10 11:24:44 +0700

    Update WiFi_Car.ino
    change HTML and the way it handle PID and car movement

--- WiFi_Car.ino
+++ WiFi_Car.ino
@@ -3,30 +3,20 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <PID_v1.h>
+#include <ArduinoJson.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-
-//Action taken
-#define UP 1
-#define DOWN 2
-#define STOP 0
-
-//motor movement action
-#define FORWARD 1
-#define BACKWARD -1
-
-int speedVal = 0;               //0-10 from the webserver input
-int motorPWM = 0;               //map speedVal from 0-255 for pwm magnitude
 volatile int encoderValue = 0;  //value of encoder
 float distanceValue = 0.0;      //distance traveled in cm
 
 //PID controller
+int out = 0;
+long unsigned int time_;
 double target = 100.0;
 double Setpoint, Input, Output;
-double Kp = 1.0, Ki = 1.0, Kd = 1.0;
+double Kp = 0.8, Ki = 0.5, Kd = 0.9;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-double difference = 0.0;
 
 // Pin definitions
 
@@ -48,15 +38,10 @@ double difference = 0.0;
 #define CIR 45.553055  //Circumference of the wheel
 #define PPR 900.0      //pulse per rotation
 
-//Create task to pin to cores
-TaskHandle_t Task1;
-TaskHandle_t Task2;
-
 //create LCD object
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 //ip address
 IPAddress IP;
-
 
 /*        ENCODER     */
 
@@ -98,24 +83,18 @@ void checkSerial1() {
     Kp = values[0].toFloat();
     Ki = values[1].toFloat();
     Kd = values[2].toFloat();
-    target = values[3].toFloat();
-    Serial.print("New Parameters    : ");
-    Serial.print(Kp);
-    Serial.print(",");
-    Serial.print(Ki);
-    Serial.print(",");
-    Serial.print(Kd);
-    Serial.print(",");
-    Serial.println(target);
-    
+    Setpoint = values[3].toFloat();
+
     //confirmation through the lcd
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Data received:");
     lcd.setCursor(0, 1);
-    lcd.print(String(Kp)+","+String(Ki)+","+String(Kd)+",");
+    lcd.print(String(Kp) + "," + String(Ki) + "," + String(Kd));
   }
 }
+
+
 
 /*        PID Control     */
 
@@ -125,16 +104,31 @@ void checkSerial1() {
 
 int PIDcontrol(double target) {
   myPID.SetOutputLimits(-255.0, 255.0);
+  myPID.SetSampleTime(100);
+  distanceValue = encToDistance(encoderValue);
   Setpoint = target;
   Input = distanceValue;
   myPID.Compute();
-  // Serial.println(Output);
-  return abs(Output);
+  // Serial.print(Kp);
+  // Serial.print(" ");
+  // Serial.print(Ki);
+  // Serial.print(" ");
+  // Serial.print(Kd);
+  // Serial.print(" ");
+  // Serial.print(Setpoint);
+  // Serial.print(" ");
+  // Serial.print(Input);
+  // Serial.print(" ");
+  Serial.println(Output);
+
+  return Output;
 }
 
 /*        Server     */
 
 //WiFi name and password
+// const char *ssid = "Tselhome-6DD8";
+// const char *password = "80962622";
 const char *ssid = "Group1";
 const char *password = "12345678";
 
@@ -149,25 +143,71 @@ const char *htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
   <head>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <style>
-    .state {
-     font-size: 1.5rem;
-     color:#8c8c8c;
-     font-weight: bold;
-   }
-    .arrows {
-      font-size:70px;
-      color:gainsboro;
+    .header-wrapper {
+        text-align: center;
     }
-    td {
-      background-color:teal;
-      border-radius:25%;
-      box-shadow: 5px 5px #888888;
+    #content{
+        display: flex;
+        flex-direction: row;
+     }
+    #left {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        width: 30%;
+        padding: 0 20px;
     }
-    td:active {
-      transform: translate(5px,5px);
-      box-shadow: none; 
+    #right {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        width: 70%;
+        padding: 0 20px;
     }
-
+    #slider-container {
+        height: 300px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 0 auto;
+    }
+    #slider {
+        appearance: none;
+        transform: rotate(270deg) translateY(-50%);
+        width: 200px;
+        height: 20px;
+        background: #AAAAAA;
+        outline: none;
+        opacity: 0.7;
+        transition: opacity 0.2s;
+        position: absolute;
+        top: 50%;
+    }
+    #slider::-webkit-slider-thumb {
+        appearance: none;
+        width: 40px;
+        height: 40px;
+        background: #678dc8;
+        cursor: pointer;
+        border-radius: 50%;
+        border: 1px solid white;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    label {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+    label span {
+        margin-right: 10px;
+    }
     .noselect {
       -webkit-touch-callout: none; /* iOS Safari */
         -webkit-user-select: none; /* Safari */
@@ -181,31 +221,53 @@ const char *htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
   </head>
   <title>Group 1 WiFi Car</title>
   <body class="noselect" align="center" style="background-color:dimgrey">
-     
-    <h1 style="color: black;text-align:center;">Microcontroller Group 1</h1>
-    <h2 style="color: black;text-align:center;">Wi-Fi &#128663; Control</h2>
-    <p>Distance moved: <span id="encoderValue">0</span> cm</p>
-    <p>Speed: <span id="speedVal">0</span></p>
-    <table id="mainTable" style="width:400px;margin:auto;table-layout:fixed" CELLSPACING=10>
-      <tr>
-        <td ontouchstart='onTouchStartAndEnd("1")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8679;</span></td>
-        <td ontouchstart='onTouchStartAndEnd("3")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8679;</span></td>
-      </tr>
-      <tr>
-        <td ontouchstart='onTouchStartAndEnd("5")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >Move 1m</span></td>
-      </tr>
-      
-      <tr>
-        <td ontouchstart='onTouchStartAndEnd("2")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8681;</span></td>
-        <td ontouchstart='onTouchStartAndEnd("4")' ontouchend='onTouchStartAndEnd("0")'><span class="arrows" >&#8681;</span></td>
-      </tr>
-    </table>
+  <div class="header-wrapper">
+  <h1 style="color: black;text-align:center;">Microcontroller Group 1</h1>
+  <h2 style="color: black;text-align:center;">Wi-Fi &#128663; Control</h2>
+  </div>
+    <div id="content">
+      <div id="left">
+		<div id="slider-container">
+          <input type="range" min="-100" max="100" value="0" step="1" id="slider">
+        </div>
+      </div>
 
+      <div id="right">
+        <p>Distance moved: <span id="encoderValue">0</span> cm</p>
+        <label for="speed">Speed: <span id="speedVal">0</span> </label>
+
+          <form id="myForm">
+              <table>
+                  <tr>
+                      <td><label for="Kp">Kp:<span id="disp_Kp">0</span></label></td>
+                      <td><input type="number" id="Kp" name="Kp" step="0.01"></td>
+                  </tr>
+                  <tr>
+                      <td><label for="Ki">Ki:<span id="disp_Ki">0</span></label></td>
+                      <td><input type="number" id="Ki" name="Ki" step="0.01"></td>
+                  </tr>
+                  <tr>
+                      <td><label for="Kd">Kd:<span id="disp_Kd">0</span></label></td>
+                      <td><input type="number" id="Kd" name="Kd" step="0.01"></td>
+                  </tr>
+                  <tr>
+                      <td><label for="setPoint">Target:<span id="disp_setPoint">0</span></label></td>
+                      <td><input type="number" id="setPoint" name="setPoint" step="0.01"></td>
+                  </tr>
+                  <tr>
+                      <td colspan="2"><input type="submit" value="Run!"></td>
+                  </tr>
+              </table>
+          </form>
+
+      </div>
+    </div>
+    
+  </body>
     <script>
       var webSocketUrl = "ws:\/\/" + window.location.hostname + "/ws";
       var websocket;
-      
-      
+		
       function initWebSocket() 
       {
         websocket = new WebSocket(webSocketUrl);
@@ -214,97 +276,128 @@ const char *htmlHomePage PROGMEM = R"HTMLHOMEPAGE(
         websocket.onmessage = function(event){ 
           // Parse the received data as JSON
             let data = JSON.parse(event.data);
+            var time_ = 0;
+            var distance_=0;
             // Check if the received data contains the new encoder value
             if (data.encoderValue) {
                 // Update the innerHTML property of the encoderValue element with the new value
                 document.getElementById("encoderValue").innerHTML = data.encoderValue;
+                distance = data.encoderValue;
             }
-            if (data.speedVal) {
+            if (data.time_){
+                time_ = data.time_;
+                addDataPoint(time_, distance_);
+            }
+            if (data.disp_Kp) {
                 // Update the innerHTML property of the encoderValue element with the new value
-                document.getElementById("speedVal").innerHTML = data.speedVal;
+                document.getElementById("disp_Kp").innerHTML = data.disp_Kp;
             }
-        };
-      }
+            if (data.disp_Ki) {
+                // Update the innerHTML property of the encoderValue element with the new value
+                document.getElementById("disp_Ki").innerHTML = data.disp_Ki;
+            }
+            if (data.disp_Kd) {
+                // Update the innerHTML property of the encoderValue element with the new value
+                document.getElementById("disp_Kd").innerHTML = data.disp_Kd;
+            }
+            if (data.disp_setPoint) {
+                // Update the innerHTML property of the encoderValue element with the new value
+                document.getElementById("disp_setPoint").innerHTML = data.disp_setPoint;
+            }
 
-      function onTouchStartAndEnd(value) 
-      {
-        websocket.send(value);
-      }
-          
+        };
+      }          
       window.onload = initWebSocket;
-      document.getElementById("mainTable").addEventListener("touchend", function(event){
-        event.preventDefault()
-      });      
+      
+        const slider = document.getElementById("slider");
+        
+        slider.addEventListener("input", () => {
+        const value = slider.value;
+        // Create a JSON object to send to the server
+        const data = {sliderValue: value};
+        // Convert the JSON object to a string and send it over the WebSocket
+        websocket.send(JSON.stringify(data));
+        document.getElementById("speedVal").innerHTML = value;
+        });
+
+        slider.addEventListener("touchend", () => {
+        if (slider.value !== 0) {
+            slider.value = 0;
+            const value = slider.value;
+        // Create a JSON object to send to the server
+            const data = {sliderValue: value};
+        // Convert the JSON object to a string and send it over the WebSocket
+            websocket.send(JSON.stringify(data));
+            document.getElementById("speedVal").innerHTML = value;
+        }
+        });
+        
+        const form = document.querySelector('form');
+        form.addEventListener('submit', (event) => {
+        event.preventDefault(); // prevent the form from submitting normally
+        const data = {
+        Kp: parseFloat(document.getElementById('Kp').value),
+        Ki: parseFloat(document.getElementById('Ki').value),
+        Kd: parseFloat(document.getElementById('Kd').value),
+        setPoint: parseFloat(document.getElementById('setPoint').value)
+        };
+        websocket.send(JSON.stringify(data));
+        document.getElementById("myForm").reset();
+        });
+
     </script>
-    
-  </body>
 </html> 
 
 )HTMLHOMEPAGE";
 
+
 /*        Server -> Client     */
 
 void processCarMovement(String inputValue) {
-  Serial.printf("Got value as %s %d\n", inputValue.c_str(), inputValue.toInt());
-  switch (inputValue.toInt()) {
-
-    case UP:
-      ledcWrite(0, motorPWM);  //(channel, dutycycle)L_PWM
-      ledcWrite(1, 0);         //(channel, dutycycle)R_PWM
-
-      break;
-
-    case DOWN:
-      ledcWrite(0, 0);         //(channel, dutycycle)L_PWM
-      ledcWrite(1, motorPWM);  //(channel, dutycycle)R_PWM
-
-      break;
-
-    case 3:
-      if (speedVal < 10) {
-        speedVal = speedVal + 1;
-      }
-      break;
-
-    case 4:
-      if (speedVal > 0) {
-        speedVal = speedVal - 1;
-      }
-      break;
-    case 5:
-      myPID.SetTunings(Kp, Ki, Kd);
-      // Serial.println("PID triggered");
-      difference = target - distanceValue;
-      while (difference > 1 || difference < -1) {
-        // Serial.println("Inside the while loop");
-        
-        if (PIDcontrol(difference) > 0) {  //forward
-          ledcWrite(0, PIDcontrol(target));
-          ledcWrite(1, 0);
-        } else if (PIDcontrol(difference) < 0) {  //reverse
-          ledcWrite(0, 0);
-          ledcWrite(1, PIDcontrol(target));
-        } else {  //stop both motors
-          ledcWrite(0, 0);
-          ledcWrite(1, 0);
-        }
-        distanceValue = encToDistance(encoderValue);
-        difference = target - distanceValue;
-      }
-      break;
-
-    case STOP:
-      ledcWrite(0, 0);  //(channel, dutycycle)L_PWM
-      ledcWrite(1, 0);  //(channel, dutycycle)R_PWM
-
-      break;
-
-    default:
-      ledcWrite(0, 0);  //(channel, dutycycle)L_PWM
-      ledcWrite(1, 0);  //(channel, dutycycle)R_PWM
-
-      break;
+  int inputValue_ = inputValue.toInt();
+  //Move the Robot
+  if (inputValue_ > 0) {
+    ledcWrite(0, map(abs(inputValue_), 0, 100, 0, 255));  //(channel, dutycycle)L_PWM
+    ledcWrite(1, 0);                                      //(channel, dutycycle)R_PWM
+  } else if (inputValue_ < 0) {
+    ledcWrite(0, 0);                                      //(channel, dutycycle)L_PWM
+    ledcWrite(1, map(abs(inputValue_), 0, 100, 0, 255));  //(channel, dutycycle)R_PWM
+  } else {
+    ledcWrite(0, 0);  //(channel, dutycycle)L_PWM
+    ledcWrite(1, 0);  //(channel, dutycycle)R_PWM
   }
+}
+
+void process_PID_parameter(double Kp_, double Ki_, double Kd_, double Setpoint_) {
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  Setpoint = Setpoint_;
+
+  myPID.SetTunings(Kp, Ki, Kd);
+  distanceValue = encToDistance(encoderValue);
+  double difference = Setpoint - distanceValue;
+  while (difference > 1 || difference < -1) {
+    // time_ = millis();
+    // ws.printfAll("{\"time_\": %.d}", time_);
+    // ws.printfAll("{\"encoderValue\": %.2f}", distanceValue);
+    out = PIDcontrol(Setpoint);
+    if (out > 0) {  //forward
+      ledcWrite(0, abs(out));
+      ledcWrite(1, 0);
+    } else if (out < 0) {  //reverse
+      ledcWrite(0, 0);
+      ledcWrite(1, abs(out));
+    } else {  //stop both motors
+      ledcWrite(0, 0);
+      ledcWrite(1, 0);
+    }
+    distanceValue = encToDistance(encoderValue);
+    difference = Setpoint - distanceValue;
+    // Serial.println(difference);
+  }
+      ledcWrite(0, 0);
+      ledcWrite(1, 0);
 }
 
 void handleRoot(AsyncWebServerRequest *request) {
@@ -337,7 +430,19 @@ void onWebSocketEvent(AsyncWebSocket *server,
       if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
         std::string myData = "";
         myData.assign((char *)data, len);
-        processCarMovement(myData.c_str());
+
+        // Parse JSON data
+        DynamicJsonDocument doc(1024);
+        deserializeJson(doc, myData);
+
+        if (doc.containsKey("sliderValue")) {
+          processCarMovement(doc["sliderValue"]);
+        } else if (doc.containsKey("Kp") && doc.containsKey("Ki") && doc.containsKey("Kd") && doc.containsKey("setPoint")) {
+          process_PID_parameter(doc["Kp"], doc["Ki"], doc["Kd"], doc["setPoint"]);
+        } else {
+          // Handle unsupported JSON tag
+          Serial.println("Unsupported JSON tag");
+        }
       }
       break;
     case WS_EVT_PONG:
@@ -355,8 +460,18 @@ void setup() {
   Serial1.begin(9600, SERIAL_8N1, BLE_RX, BLE_TX);
   // Set WiFi mode to an Acess point with the previously defined ssid and password
   WiFi.softAP(ssid, password);
+
+  // WiFi.begin(ssid, password); 
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(1000);
+  //   Serial.println("Connecting to WiFi...");
+  // }
+  // Serial.println("WiFi connected");
+
   //get and print IP Adress
+  // IP = WiFi.localIP();
   IP = WiFi.softAPIP();
+
   Serial.print("AP IP address: ");
   Serial.println(IP);
 
@@ -401,59 +516,20 @@ void setup() {
 
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
-
-  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
-  // xTaskCreatePinnedToCore(
-  //                   Task1code,   /* Task function. */
-  //                   "Task1",     /* name of task. */
-  //                   10000,       /* Stack size of task */
-  //                   NULL,        /* parameter of the task */
-  //                   1,           /* priority of the task */
-  //                   &Task1,      /* Task handle to keep track of created task */
-  //                   0);          /* pin task to core 0 */
-  // delay(500);
-
-  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
-  xTaskCreatePinnedToCore(
-    Task2code, /* Task function. */
-    "Task2",   /* name of task. */
-    10000,     /* Stack size of task */
-    NULL,      /* parameter of the task */
-    1,         /* priority of the task */
-    &Task2,    /* Task handle to keep track of created task */
-    1);        /* pin task to core 1 */
-  delay(500);
-}
-
-
-// void Task1code( void * pvParameters ){
-//   Serial.print("Task1 running on core ");
-//   Serial.println(xPortGetCoreID());
-// for(;;){
-
-// }
-// }
-
-void Task2code(void *pvParameters) {
-  Serial.print("Task2 running on core ");
-  Serial.println(xPortGetCoreID());
-
-  for (;;) {
-    //Map the speedVal that we obtain from the webserver client to an 8 bit value
-    motorPWM = map(speedVal, 0, 10, 0, 255);
-    checkSerial1();  //update PID parameters
-
-    // Serial.println(encoderValue);
-    // Serial.println(motorPWM);
-    //update the encoderValue and speedVal to the webserver
-    distanceValue = encToDistance(encoderValue);
-    ws.printfAll("{\"encoderValue\": %.2f}", distanceValue);
-    ws.printfAll("{\"speedVal\": %d}", speedVal);
-    //free resources of disconected clients
-    ws.cleanupClients();
-    delay(100);
-  }
 }
 
 void loop() {
+  //Map the speedVal that we obtain from the webserver client to an 8 bit value
+  checkSerial1();  //update PID parameters
+
+  //update the encoderValue and speedVal to the webserver
+  distanceValue = encToDistance(encoderValue);
+  ws.printfAll("{\"encoderValue\": %.2f}", distanceValue);
+  ws.printfAll("{\"disp_Kp\": %.2f}", Kp);
+  ws.printfAll("{\"disp_Ki\": %.2f}", Ki);
+  ws.printfAll("{\"disp_Kd\": %.2f}", Kd);
+  ws.printfAll("{\"disp_setPoint\": %.2f}", Setpoint);
+  //free resources of disconected clients
+  ws.cleanupClients();
+  delay(100);
 }
